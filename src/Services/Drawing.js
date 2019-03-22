2d2beb8b910682957fbf1e3f9ba241479448311e
ithespv <ithespv@gmail.com>
2019-03-22 19:37:50 +0200

    fix retina pixel ratio

--- src/Services/Drawing.js
+++ src/Services/Drawing.js
@@ -1,4 +1,6 @@
 class Drawing {
+  scale = window.devicePixelRatio
+
   /**
    *
    * @param {HTMLElement} canvas
@@ -7,16 +9,24 @@ class Drawing {
    */
   constructor (canvas, width, height) {
     this.canvas = canvas
+    this.canvas.style.setProperty('--height', `${height}px`)
+    this.canvas.style.setProperty('--width', `${width}px`)
+
     this.height = height
     this.width = width
+
     this.ctx = this.canvas.getContext('2d')
-    this.canvas.height = height
-    this.canvas.width = width
+    this.ctx.scale(this.scale, this.scale)
+
+    this.canvas.height = this.getScale(height)
+    this.canvas.width = this.getScale(width)
   }
 
   clearCanvas () {
-    this.ctx.clearRect(0, 0, this.width, this.height)
+    this.ctx.clearRect(0, 0, this.getScale(this.width), this.getScale(this.height))
   }
+
+  getScale = value => value * this.scale
 
   /**
    * Draw a line from the start coordinates to the end coordinates
@@ -27,11 +37,11 @@ class Drawing {
    */
   drawALine (start, end, color = 'rgba(0, 0, 0, 0.8)', lineWidth = 2) {
     this.ctx.strokeStyle = color
-    this.ctx.lineWidth = lineWidth
+    this.ctx.lineWidth = this.getScale(lineWidth)
 
     this.ctx.beginPath()
-    this.ctx.moveTo(...start.value)
-    this.ctx.lineTo(...end.value)
+    this.ctx.moveTo(...start.value.map(this.getScale))
+    this.ctx.lineTo(...end.value.map(this.getScale))
     this.ctx.stroke()
     // this.ctx.fillText(start.value[1], ...start.value)
   }
@@ -45,7 +55,7 @@ class Drawing {
   drawADot (coordinates, radius, color = 'rgba(255, 153, 255, 0.8)') {
     this.ctx.fillStyle = color
     this.ctx.beginPath()
-    this.ctx.arc(...coordinates.value, radius, 0, 2 * Math.PI)
+    this.ctx.arc(...coordinates.value.map(this.getScale), radius, 0, 2 * Math.PI)
     this.ctx.fill()
   }
 }
